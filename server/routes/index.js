91746a0149d02ad00520f0ddd4d62ffddb301f0e
Rusty Zarse <rusty@levo.us>
2017-01-23 01:27:18 -0500

    fixed route for icon

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,11 @@
+const path = require('path');
 var express = require('express');
 
 exports.setup = function (basePath, app){
 
   const router = express.Router();
   router.get('/apple-touch-icon.png', (req, res) => {
-    response.sendFile(path.resolve(__dirname, 'server', 'static', 'images', 'apple-touch-icon.png'));
+    res.sendFile(path.resolve(__dirname, '../', 'static', 'images', 'apple-touch-icon.png'));
   });
 
   app.use(basePath, router);
