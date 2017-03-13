13ee72b1ca49e2cd8ad779748ce987563706434b
Rusty Zarse <rusty@levo.us>
2017-03-13 14:13:41 -0400

    added avatar to member list that uses gravatar or initials

--- client/src/components/membership/MemberList.jsx
+++ client/src/components/membership/MemberList.jsx
@@ -1,6 +1,7 @@
 import React, {PropTypes, Component} from 'react';
 import { List, ListItem } from 'material-ui/List';
-
+import Avatar from 'react-avatar';
+import md5 from 'js-md5';
 //TODO: convert styles.listStyle to className
 
 export default class MemberList extends Component {
@@ -33,10 +34,14 @@ export default class MemberList extends Component {
 
     return (
       <div style={styles.listStyle} className={className}>
+
         <List>
           {
             this.props.members.map((member, i) =>
               <ListItem key={`mem${i}`}
+                leftAvatar={
+                  <Avatar md5Email={md5(member.email)} name={`${member.firstName} ${member.lastName}`} round={true} size={40} />
+                }
                 primaryText={`${member.firstName} ${member.lastName}`}
                 secondaryText={member.propertyAddress && member.propertyAddress.street}
                 onTouchTap={() => this.handleItemTouchTap(member)}
